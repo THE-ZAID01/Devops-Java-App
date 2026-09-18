cb029bf669e9ed87bd6482f0fd891eeb82e9420d
THE_ZAID <zmulla137@gmail.com>
2026-09-19 00:44:50 +0530

    Implement add method

---
+++ Add.java
@@ -0,0 +1,11 @@
+public class Add
+{
+  public static int add(int a,int b)
+    return a+b;
+
+  public static void main(String[] args)
+    {
+        System.out.println("Hello Devops!!!");
+				System.out.println("2+3= "+add(2,3));
+    }
+}
